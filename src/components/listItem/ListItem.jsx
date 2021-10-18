8b9a1cbfdfae2cfcd8d5b4393b5d4366a5a93bad
Ashish Maharjan <amaharjan1032@gmail.com>
2021-10-18 18:36:29 +0545

    Slider part done

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,21 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons";
+import "./listitem.scss";
+
+export default function ListItem() {
+    return (
+        <div className="listItem">
+            <img
+                src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+                alt=""
+            />
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow className="icon" />
+                    <Add className="icon" />
+                    <ThumbUpAltOutlined className="icon" />
+                    <ThumbDownAltOutlined className="icon" />
+                </div>
+            </div>
+        </div>
+    );
+}
