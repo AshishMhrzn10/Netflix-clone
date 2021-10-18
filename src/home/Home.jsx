966d21c5e59ef1533440e535ba7d8bf2a0533c16
Ashish Maharjan <amaharjan1032@gmail.com>
2021-10-18 16:49:13 +0545

    Featured part done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
@@ -5,11 +6,7 @@ const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <img
-                width="100%"
-                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-            />
+            <Featured />
         </div>
     );
 };
