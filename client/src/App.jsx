78c9aded22bde0f5c3ed925f464f54fef734feb8
Ashish Maharjan <amaharjan1032@gmail.com>
2021-10-28 15:12:14 +0545

    Movie API done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
-  return <Register />;
+  return <Home />;
 };
 
 export default App;
