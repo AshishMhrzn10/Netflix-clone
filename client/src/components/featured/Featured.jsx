997da1dcce928a37da5bdaf8817b7b48b67143df
Ashish Maharjan <amaharjan1032@gmail.com>
2021-10-30 15:19:47 +0545

    Featured dome

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,7 +1,27 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import { useEffect, useState } from "react";
 import "./featured.scss";
+import axios from "axios";
 
 export default function Featured({ type }) {
+    const [content, setContent] = useState({});
+
+    useEffect(() => {
+        const getRandomContent = async () => {
+            try {
+                const res = await axios.get(`/movies/random?type=${type}`,
+                    {
+                        headers: {
+                            token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2E0M2E2ZjcyNzU0YmUyYWI5Y2M1OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNTUyMjY2NCwiZXhwIjoxNjM1OTU0NjY0fQ.VK_jNiBK-BkwSFrBcTDwke4a7nEN0EvU-kQoJMe5S6Y"
+                        }
+                    });
+                setContent(res.data[0]);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getRandomContent();
+    }, [type]);
     return (
         <div className="featured">
             {type && (
@@ -25,17 +45,14 @@ export default function Featured({ type }) {
                     </select>
                 </div>
             )}
-            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <img src={content.imgSm} />
             <div className="info">
                 <img
-                    src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+                    src={content.imgTitle}
                     alt=""
                 />
                 <span className="desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                    adipisci repellendus eum quasi illo, velit numquam, maxime tempora
-                    sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
-                    temporibus eum earum?
+                    {content.desc}
                 </span>
                 <div className="buttons">
                     <button className="play">
